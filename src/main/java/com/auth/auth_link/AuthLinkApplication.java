865418858f7e7f745c712864c404802e24e940db
지성배 <wltjdqo55@gmail.com>
2024-06-14 10:19:56 +0900

    first commit

---
+++ src/main/java/com/auth/auth_link/AuthLinkApplication.java
@@ -0,0 +1,13 @@
+package com.auth.auth_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auth/auth_link/AuthLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.auth_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
